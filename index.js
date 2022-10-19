10d0175714ff053934e75dea63237f5137d19c7d
agwadan <agwa.dan@gmail.com>
2022-10-19 09:47:16 +0300

    Complete creation of a callback hell

--- index.js
+++ index.js
@@ -13,7 +13,33 @@ let order = (fruit_name, call_production) => {
 }
 
 let production = () => {
-  setTimeout(() => { console.log("Production has started"); }, 1000)
+  setTimeout(() => {
+    console.log("Production has started");
+
+    setTimeout(() => {
+      console.log(`The Fruit has been chopped`);
+
+      setTimeout(() => {
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} were added.`);
+
+        setTimeout(() => {
+          console.log('The machine has been started');
+
+          setTimeout(() => {
+            console.log(`${stocks.holder[0]} will hold the ice cream`);
+
+            setTimeout(() => {
+              console.log(`${stocks.toppings[0]} was added.`);
+
+              setTimeout(() => {
+                console.log('Serve Ice cream : )');
+              }, 2000)
+            }, 3000)
+          }, 2000)
+        }, 1000)
+      }, 1000)
+    }, 2000)
+  }, 1000)
 }
 
 order(0, production)
