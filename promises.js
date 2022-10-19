0951578ab8d2a75f031551ccd228c40568ff40cf
agwadan <agwa.dan@gmail.com>
2022-10-19 11:30:06 +0300

    Complete promises

---
+++ promises.js
@@ -0,0 +1,32 @@
+let stocks = {
+  Fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"],
+};
+
+let is_shop_open = true;
+
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (is_shop_open) {
+      setTimeout(() => {
+        resolve(work());
+      }, time)
+
+    } else {
+      reject(console.log(`Our shop is closed.`))
+    }
+  })
+}
+
+order(2000, () => console.log(`${stocks.Fruits[3]} has been selected`))
+  .then(() => { return order(0, () => console.log(`Production has started`)) })
+  .then(() => { return order(2000, () => console.log('The fruit was chopped')) })
+  .then(() => { return order(1000, () => console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} were selected`)) })
+  .then(() => { return order(1000, () => console.log("Start Machine")) })
+  .then(() => { return order(2000, () => console.log(`ice cream was placed on ${stocks.holder[2]}`)) })
+  .then(() => { return order(3000, () => console.log(`${stocks.toppings[1]} was selected for the topping.`)) })
+  .then(() => { return order(2000, () => console.log("Ice cream was served")) })
+  .catch(() => console.log("Customer left"))
+  .finally(() => console.log("Day ended . . . the shop is closed")) // Will run whether the promise is resolved or rejected.
